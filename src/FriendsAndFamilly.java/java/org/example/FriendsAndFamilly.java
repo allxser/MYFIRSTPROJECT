23bb649d631bb980b7a95c4dedf6b9fc45904091
MYFIRSTPROJECT <allxser@hotmail.com>
2022-07-20 20:58:07 +0300

    first commit

---
+++ src/FriendsAndFamilly.java/java/org/example/FriendsAndFamilly.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class FriendsAndFamilly {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        do {
+            System.out.println("\n Enter list price: ");
+            double listPrice = input.nextDouble();
+
+            System.out.println(" Enter discount %: ");
+            int discount = input.nextInt();
+
+            System.out.printf(" You'll pay only rub%2.2f", listPrice + listPrice * discount / 100);
+
+        } while (true);
+    }
+}
+
